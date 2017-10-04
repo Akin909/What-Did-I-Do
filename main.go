904c57526f810a7d98e61ae4eb3b8248558fff10
Akin909 <akin.sowemimo@gmail.com>
2017-10-05 00:10:26 +0100

    add terminal widgets

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	ui "github.com/gizak/termui"
 )
 
-func main() {
+func gitCommand() (out string) {
 
 	var (
 		err    error
@@ -23,4 +25,25 @@ func main() {
 	sha := string(cmdOut)
 	firstSix := sha[:6]
 	fmt.Println("The first six chas of the SHA at HEAD in this repo are", firstSix)
+	return "The first six chas of the SHA at HEAD in this repo are" + firstSix
+}
+
+func quit(ui.Event) {
+	ui.StopLoop()
+}
+
+func main() {
+	err := ui.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer ui.Close()
+
+	p, g := quitMenu()
+	output := gitCommand()
+	git := showGitOutput(output)
+	ui.Render(p, g, git)
+	ui.Loop()
+	// Handle q to quit
+	ui.Handle("/sys/kbd/q", quit)
 }
